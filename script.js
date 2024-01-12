1bfd4a816ed464d753730a2b8455d100991cf86e
rogerc37 <145470030+rogerc37@users.noreply.github.com>
2024-01-12 11:02:20 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,20 +1,65 @@
 var dice1 = 1;
 var dice2 = 2;
 var dice3 = 3;
+var numberrolled = 1;
 var total = 6;
-var mean = 2;
-var median = 2;
+var mean = 0;
+var median = 0;
 var mode = 1;
 var doubles = 0;
+var propdoubles = 0;
 var triples = 0;
-var cmean = 2;
-var cmedian = 2;
-var cmode = 1;
+var proptriples = 0;
+var cmean = 0;
+var cmedian = 0;
+var cmode = 0;
 var totalrolls = 0;
-const rolls = [1,1,1,2,23,3,3,43,43];
+const rolls = [];
 function rolldice()
 {
-    // rolls the dice
+    //ROLLS THE DICE/DIE
+    var trials = document.getElementById("trials").form.id;
+    numberrolled = document.getElementById("numrolls").form.id;
+    for(let i = 0; i<trials; i++)
+    {
+        if (numberrolled === 1)
+        {
+            dice1 = Math.floor(Math.random()*(7 - 1) + 1);
+            totalrolls += 1;
+            total = dice1;
+            mean = getmean(numrolled, dice1, null, null);
+            median = getmedian(numrolled, dice1, null, null);
+            mode = getmode(numrolled, dice1, null, null);
+        }
+        else if (numberrolled === 2)
+        {
+            dice1 = Math.floor(Math.random()*(7 - 1) + 1);
+            dice2 = Math.floor(Math.random()*(7 - 1) + 1);
+            totalrolls += 2;
+            total = dice1 + dice2;
+            mean = getmean(numrolled, dice1, dice2, null);
+            median = getmedian(numrolled, dice1, dice2, null);
+            mode = getmode(numrolled, dice1, dice2, null);
+            propdoubles = getdoubles(numberrolled, dice1, dice2, null);
+        }
+        else
+        {
+            dice1 = Math.floor(Math.random()*(7 - 1) + 1);
+            dice2 = Math.floor(Math.random()*(7 - 1) + 1);
+            dice3 = Math.floor(Math.random()*(7 - 1) + 1);
+            totalrolls += 3;
+            total = dice1 + dice2 + dice3;
+            mean = getmean(numrolled, dice1, dice2, dice3);
+            median = getmedian(numrolled, dice1, dice2, dice3);
+            mode = getmode(numrolled, dice1, dice2, dice3);
+            propdoubles = getdoubles(numberrolled, dice1, dice2, dice3);
+            proptriples = gettriples(dice1, dice2, dice3);
+        }
+        cmean = getcmean();
+        cmedian = getcmedian();
+        cmode = getcmode();
+        updatetable(numberrolled, total, mean, median, mode, propdoubles, proptriples, cmean, cmedian, cmode);
+    }
 }
 function getmean(numrolled, r1, r2, r3)
 {
@@ -40,13 +85,24 @@ function getmode(numrolled, r1, r2, r3)
     }
     else{return r1;}
 }
-function getdoubles(r1, r2, r3)
+function getdoubles(numrolled, r1, r2, r3)
 {
     // gets the doubles over the total rolls
-    if((r1 === r2)||(r1 === r3)||(r2 === r3))
+    if (numrolled === 2)
     {
-        doubles += 1;
-        return doubles/totalrolls;
+        if(r1 === r2)
+        {
+            doubles += 1;
+            return doubles/totalrolls;
+        }
+    }
+    else
+    {
+        if((r1 === r2)||(r1 === r3)||(r2 === r3))
+        {
+            doubles += 1;
+            return doubles/totalrolls;
+        }
     }
     return doubles/totalrolls;
 }
@@ -80,12 +136,15 @@ function getcmedian()
 }
 function getcmode()
 {
-    {
-        console.log( rolls.sort((a,b) => rolls.filter(v => v===a).length - rolls.filter(v => v===b).length).pop());
-    }
+    //gets cumulative mode
+    return rolls.sort((a,b) => rolls.filter(v => v===a).length - rolls.filter(v => v===b).length).pop();
 }
 function updatetable(numrolled, total, mean, median, mode, doubles, triples, cmean, cmedian, cmode)
 {
     //updates the table
 }
-getcmode();
+while(totalrolls<=10){
+    rolldice();
+    totalrolls+=1;
+}
+
